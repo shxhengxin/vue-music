79fde69df5792be721ad4bb3186cf640b6528529
shenhengxin <shenhengxin@qq.com>
2017-10-31 02:16:15 +0800

    使用jsonp获取数据

--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,13 @@
-/**
- * Created by Administrator on 2017/10/30 0030.
- */
+export const commonParams = {
+  g_tk: 1928093487,
+  inCharset: 'utf-8',
+  outCharset: 'utf-8',
+  notice: 0,
+  format: 'jsonp'
+}
+
+export const options = {
+  param: 'jsonpCallback'
+}
+
+export const ERR_OK = 0
